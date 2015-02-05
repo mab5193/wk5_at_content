fac2401ba86989610741e54c05bdd157a8f82a85
Melissa <mab5193@ucla.edu>
2015-02-05 03:08:41 +0000

    Remove N/n characters for DNA sequences

---
+++ at_content_improved_bonus.py
@@ -0,0 +1,16 @@
+from __future__ import division # for python 2
+def get_at_content(dna): # define function get_at_content for variable 'dna'
+    dna = dna.replace('N', '') 
+    dna = dna.replace('n', '') #remove N/n characters 
+    length = len(dna) # define length as length of dna
+    a_count = dna.upper().count('A') # define a_count as occurances of A
+    t_count = dna.upper().count('T') # define t_count as occurances of T
+    #convert sequence to upper case before counting
+    at_content = (a_count + t_count) / length #calculate at content by (A+T)/2
+    return round (at_content, 2)  # return with 2 sig figs
+
+#example for 3 function calls
+my_at_content = get_at_content("ATGCGCGATCGATCGAATCG")
+print(str(my_at_content)) 
+print(get_at_content("ATGCATGCAACTGTAGC")) 
+print(get_at_content("aactgtagctagctagcagcgta")) 
